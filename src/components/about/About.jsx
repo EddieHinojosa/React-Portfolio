c5d8b9377e16f8f7ca0ac14d69f3a23115b9e97f
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-21 05:29:16 -0400

    added animated carousel

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,7 +10,6 @@ import blenderIcon from '../../assets/blender.png';
 
 
 
-
 const About = () => {
     return (
         <section id="about-me" className="container my-5">
@@ -20,35 +19,23 @@ const About = () => {
                 </div>
                 <div className="col-md-8">
                     <h2>About Me</h2>
-                    <p>My name is Eddie Hinojosa, I am a Full-Stack Developer as of 2024, my prior work experience has been</p>
+                    <p>My name is Eddie Hinojosa, I am a Full-Stack Developer as of 2024, my prior work experience has been.</p>
                     <p>Another paragraph about yourself...</p>
                 </div>
             </div>
             <div className="row mt-4">
                 <div className="col-12">
-                    <div id="skillsCarousel" className="carousel slide" data-bs-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={htmlIcon} className="d-block mx-auto" alt="HTML" />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={cssIcon} className="d-block mx-auto" alt="CSS" />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={jsIcon} className="d-block mx-auto" alt="JavaScript" />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={blenderIcon} className="d-block mx-auto" alt="Blender" />
-                            </div>
+                    <div className="skills-carousel">
+                        <div className="carousel-track">
+                            <img src={htmlIcon} alt="HTML" />
+                            <img src={cssIcon} alt="CSS" />
+                            <img src={jsIcon} alt="JavaScript" />
+                            <img src={blenderIcon} alt="Blender" />
+                            <img src={htmlIcon} alt="HTML" />
+                            <img src={cssIcon} alt="CSS" />
+                            <img src={jsIcon} alt="JavaScript" />
+                            <img src={blenderIcon} alt="Blender" />
                         </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#skillsCarousel" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#skillsCarousel" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
                     </div>
                 </div>
             </div>
