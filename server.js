3a6a4b4e04c26623820cbc9f07d584c6b87edf66
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-22 22:49:52 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+//server.js used to run on netlify.com
+
+//import express
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
